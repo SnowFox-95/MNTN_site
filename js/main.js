63e6118a760d5e0d36430481b5d0131e1cd9db56
SnowFox-95 <321mts95@gmail.com>
2022-09-10 19:20:10 +0300

    10

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
+const body = document.body;
 
 navBtn.addEventListener('click', function() {
     mobileNav.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
 })
