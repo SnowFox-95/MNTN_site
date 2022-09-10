31a984c7a4f7163159e56ef1896be2ce6412e42c
SnowFox-95 <321mts95@gmail.com>
2022-09-10 19:30:42 +0300

    12

--- js/main.js
+++ js/main.js
@@ -2,20 +2,26 @@ const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
 const body = document.body;
 
-navBtn.addEventListener('click', function() {
+// клик по кнопке
+navBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
     mobileNav.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
     body.classList.toggle('no-scroll');
 })
 
+// клик по окну за пределами навигации
 window.addEventListener('click', function(){
     console.log('click on window');
 
-    if() {
-
+    if(body.classList.contains('no-scroll')) {
+        body.classList.toggle('no-scroll');
+        navBtn.classList.toggle('nav-button-close');
+        mobileNav.classList.toggle('mobile-nav-active');
     }
 })
 
+// останавливаем клик внутри открытой мобильной навигации
 mobileNav.addEventListener('click', function(event){
     event.stopPropagation()
 })
